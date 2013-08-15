dd57008dcd0f2a28876405f1beee812a35c71b24
Ife-Owoyemi <cyberife7@gmail.com>
2013-08-14 20:09:22 -0500

    work to hav git pull working. -Ife

--- db/migrate/20130815004300_add_new_columns_to_user.rb
+++ db/migrate/20130815004300_add_new_columns_to_user.rb
@@ -1,4 +1,17 @@
 class AddNewColumnsToUser < ActiveRecord::Migration
-  def change
-  end
+  
+	def up
+		add_column :users, :notesToFresh, :text
+		add_column :users, :notesToMym, :text
+		add_column :users, :matricuYear, :integer
+		add_column :users, :postGradPlans, :string		
+	end
+
+	def down 
+		remove_column :users, :notesToFresh
+		remove_column :users, :notesToMym
+		remove_column :users, :matricuYear
+		remove_column :users, :postGradPlans
+	end
+
 end
